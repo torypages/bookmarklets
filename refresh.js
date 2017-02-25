ee57c44b670ebce9d196c66a6b1909e9082766ad
Tory Law <git@torypages.com>
2017-02-25 13:35:19 -0500

    initial commit

---
+++ refresh.js
@@ -0,0 +1,8 @@
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
+
+
+window.location.href = "./refresh.html?url=" + b64EncodeUnicode(window.location.href)
